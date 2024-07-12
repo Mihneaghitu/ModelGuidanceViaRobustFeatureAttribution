400ce9f1f2a21e099adbdf268c9c54da2b178f2f
psosnin <philipsosnin@gmail.com>
2024-07-12 16:43:49 +0100

    fix(logging): don't add handler to root logger with basicConfig

--- abstract_gradient_training/__init__.py
+++ abstract_gradient_training/__init__.py
@@ -13,8 +13,9 @@ from abstract_gradient_training.certified_training import (
 from abstract_gradient_training import test_metrics
 from abstract_gradient_training import bounds
 
-logging.basicConfig(
-    level=logging.INFO,
-    format="[AGT] [%(levelname)-8s] [%(asctime)s] %(message)s",
-    datefmt="%H:%M:%S",
-)
+logger = logging.getLogger("abstract_gradient_training")
+logger.handlers.clear()
+formatter = logging.Formatter("[AGT] [%(levelname)-8s] [%(asctime)s] %(message)s", datefmt="%H:%M:%S")
+sh = logging.StreamHandler()
+sh.setFormatter(formatter)
+logger.addHandler(sh)
